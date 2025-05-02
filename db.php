b6c983baacc5db3133d69dfddefb82c97ab3c358
Joshualaycoh <your_email@example.com>
2025-05-02 08:22:36 +0800

    change db config

--- db.php
+++ db.php
@@ -1,13 +1,6 @@
 <?php
-$host = "sql206.infinityfree.com";
-$user = "if0_38706068";
-$password = "Col323shepperd";
-$dbname = "if0_38706068_onlineshopdb";
-
-$conn = new mysqli($host, $user, $password, $dbname);
-
+$conn = new mysqli('localhost', 'root', '', 'onlineshopdb');
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully!";
+    echo "$conn->connect_error";
 }
+          
